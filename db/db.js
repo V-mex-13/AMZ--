0d5bba115f571586f193914595187f2773d3d8c9
v-mex-13 <meetvaghela3112@gmail.com>
2025-03-21 18:44:41 +0530

    crete egnor file

--- db/db.js
+++ db/db.js
@@ -1,9 +1,11 @@
 
 const mongoose = require('mongoose')
+const url  = require('../routes/dB.JS')
+
 // mongodb+srv://<db_username>:<db_password>@cluster0.z90dk.mongodb.net/
 // mongodb+srv://meetvaghela:123@for.qxwf7.mongodb.net/
 // makig schemsa stap-1
-mongoose.connect("mongodb+srv://meetvaghela:123@for.qxwf7.mongodb.net/")
+mongoose.connect(url)
 .then(()=>console.log("conected"))
 .catch((eror)=>console.log(`tha ${eror}`))
 
